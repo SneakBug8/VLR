4a7b4fcabd2b4a503f70d92fa9e2609bc9ad6456
SneakBug8 <pavlikelf@gmail.com>
2016-12-30 21:55:03 +0300

    Basic 0.2

--- examples/basic/game.js
+++ examples/basic/game.js
@@ -6,10 +6,6 @@ addaction("Action1","act")
 addaction("Action2","act")
 addaction("Action3","act")
 times=0
-if (dismuss==0)
-{
-addaction("Выключить музыку","disablemusic")
-}
 });
 
 function act()
