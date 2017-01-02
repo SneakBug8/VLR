a4133666847c60ffd04f4244addec0f27b696e2a
SneakBug8 <pavlikelf@gmail.com>
2017-01-02 22:11:39 +0300

    Commenting

--- examples/vlr/settings.js
+++ examples/vlr/settings.js
@@ -16,7 +16,7 @@ MUSACT=4// Id действия с отключением музыки
 TIMER=1 // Требуется ли выполнение функции reload() каждые 10 секунд
 
 dismuss=0 // =1, если хотите отключить музыку
-
-REDIRECT_W=400
-REDIRECT_H=400
-MOBILE_ADR="http://www.etrt.ru/kril/mob.html"
+// Переадресация на моб версию.
+REDIRECT_W=400 // Макс Ширина для редиректа
+REDIRECT_H=400 // Макс Высота для редиректа
+MOBILE_ADR="http://www.etrt.ru/kril/mob.html" // Адрес для редиректа
